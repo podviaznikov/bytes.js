de95961d1f75f40ec3f325f5426fc78da8f3636f
Anton Podviaznikov <podviaznikov@gmail.com>
2011-08-04 20:23:54 +0200

    not worling code.

--- bytes.js
+++ bytes.js
@@ -1,6 +1,28 @@
 "use strict";
-//inspired by https://github.com/vjeux/jsDataView
-var Bytes=Object.create(DataView,{
+//heavily inspired by https://github.com/vjeux/jsDataView
+var Bytes = Object.create({},{
+    create:{
+        value:function(buffer,byteOffset,byteLength){
+            var bytes = Object.create(Bytes);
+            bytes.offset = byteOffset || 0;
+            bytes.length = byteLength || buffer.byteLength;
+            bytes._view = new DataView(buffer,bytes.offset,bytes.length);
+            return bytes;
+        }
+    },
+//    offset:{
+//        get:function(){
+//            return this._offset;
+//        },
+//        set:function(offset){
+//            this._offset = offset;
+//        }
+//    },
+//    length:{
+//        get:function(){
+//            return this.length;
+//        }
+//    },
     getChar:{
         value:function(byteOffset){
             return String.fromCharCode(this.getUint8(byteOffset));
@@ -8,13 +30,67 @@ var Bytes=Object.create(DataView,{
     },
     getString:{
         value:function(length, byteOffset){
-//            // Use Int8Array and String.fromCharCode to extract a string
-//			var int8array = new Int8Array(this._buffer, this._start + byteOffset, length);
-//			var stringarray = [];
-//			for (var i = 0; i < length; ++i) {
-//				stringarray[i] = int8array[i];
-//			}
-//			value = String.fromCharCode.apply(null, stringarray);
+			var charCodes = [],
+			    i = byteOffset || 0;
+			for (; i < length+byteOffset; ++i){
+			    console.log(i);
+				charCodes[i] = String.fromCharCode(this.getInt8(i));
+				console.log(String.fromCharCode(this.getInt8(i)));
+			}
+			// Use Int8Array and String.fromCharCode to extract a string
+			return charCodes.join('');
+        }
+    },
+    getInt8:{
+        value:function(byteOffset){
+            return this._view.getInt8(byteOffset);
+        }
+    },
+    getUint8:{
+        value:function(byteOffset){
+            return this._view.getUint8(byteOffset);
+        }
+    },
+    getInt16:{
+        value:function(byteOffset,littleEndian){
+            return this._view.getInt16(byteOffset,littleEndian);
+        }
+    },
+    getUint16:{
+        value:function(byteOffset,littleEndian){
+            return this._view.getUint16(byteOffset,littleEndian);
+        }
+    },
+    getInt32:{
+        value:function(byteOffset,littleEndian){
+            return this._view.getInt32(byteOffset,littleEndian);
+        }
+    },
+    getUint32:{
+        value:function(byteOffset,littleEndian){
+            return this._view.getUint32(byteOffset,littleEndian);
+        }
+    },
+    getFloat32:{
+        value:function(byteOffset,littleEndian){
+            return this._view.getFloat32(byteOffset,littleEndian);
+        }
+    },
+    getFloat64:{
+        value:function(byteOffset,littleEndian){
+            return this._view.getFloat64(byteOffset,littleEndian);
+        }
+    },
+
+    createBuffer:{
+        value:function(){
+            var buffer = new ArrayBuffer(arguments.length),
+                view = new Int8Array(buffer),
+                i = 0;
+            for (; i < arguments.length; ++i){
+                view[i] = arguments[i];
+            }
+            return buffer;
         }
     }
 });
