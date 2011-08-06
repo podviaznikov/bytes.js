a08a330aa29068517460076dce1c9cd0111f6f89
Anton Podviaznikov <podviaznikov@gmail.com>
2011-08-06 21:29:20 +0200

    first working version.

--- bytes.js
+++ bytes.js
@@ -1,8 +1,11 @@
 "use strict";
-//heavily inspired by https://github.com/vjeux/jsDataView
+// Main object that should be used for bytes manipulations.
 var Bytes = Object.create({},{
+    // version of the library
+    version:{value:'0.1'},
+    // create view over ArrayBuffer with some handy functions.
     create:{
-        value:function(buffer,byteOffset,byteLength){
+        value:function(buffer, byteOffset, byteLength){
             var offset = byteOffset || 0,
                 length = byteLength || buffer.byteLength,
                 view = new DataView(buffer, offset, length),
@@ -20,16 +23,18 @@ var Bytes = Object.create({},{
             return bytes;
         }
     },
+    // method for getting character from ArrayBuffer at specified offset.
     getChar:{
         value:function(byteOffset){
             return String.fromCharCode(this.getUint8(byteOffset));
         }
     },
+    // method for getting string from ArrayBuffer at specified offset and with specified length.
     getString:{
         value:function(length, byteOffset){
 			var charCodes = [],
 			    i = byteOffset || 0;
-			for (; i < length+byteOffset; ++i){
+			for (; i < length + byteOffset; ++i){
 				charCodes[i] = String.fromCharCode(this.getInt8(i));
 			}
 			return charCodes.join('');
@@ -49,44 +54,46 @@ var Bytes = Object.create({},{
     },
     // method from data view specification
     getInt16:{
-        value:function(byteOffset,littleEndian){
-            return this.view.getInt16(byteOffset,littleEndian);
+        value:function(byteOffset, littleEndian){
+            return this.view.getInt16(byteOffset, littleEndian);
         }
     },
     // method from data view specification
     getUint16:{
-        value:function(byteOffset,littleEndian){
-            return this.view.getUint16(byteOffset,littleEndian);
+        value:function(byteOffset, littleEndian){
+            return this.view.getUint16(byteOffset, littleEndian);
         }
     },
     // method from data view specification
     getInt32:{
-        value:function(byteOffset,littleEndian){
-            return this.view.getInt32(byteOffset,littleEndian);
+        value:function(byteOffset, littleEndian){
+            return this.view.getInt32(byteOffset, littleEndian);
         }
     },
     // method from data view specification
     getUint32:{
-        value:function(byteOffset,littleEndian){
-            return this.view.getUint32(byteOffset,littleEndian);
+        value:function(byteOffset, littleEndian){
+            return this.view.getUint32(byteOffset, littleEndian);
         }
     },
     // method from data view specification
     getFloat32:{
-        value:function(byteOffset,littleEndian){
-            return this.view.getFloat32(byteOffset,littleEndian);
+        value:function(byteOffset, littleEndian){
+            return this.view.getFloat32(byteOffset, littleEndian);
         }
     },
     // method from data view specification
     getFloat64:{
-        value:function(byteOffset,littleEndian){
-            return this.view.getFloat64(byteOffset,littleEndian);
+        value:function(byteOffset, littleEndian){
+            return this.view.getFloat64(byteOffset, littleEndian);
         }
     },
-
+    // utility method for creating ArrayBuffer right from JavaScript.
     createBuffer:{
-        value:function(){
+        value:function(/* arguments */){
+            // initialize buffer
             var buffer = new ArrayBuffer(arguments.length),
+                // create view over buffer
                 view = new Int8Array(buffer),
                 i = 0;
             for (; i < arguments.length; ++i){
